8dd600d1fad181cb36f847b9fd584d620828a7c0
exonwarrior <drhaenze@gmail.com>
2014-02-05 20:58:24 +0000

    Initial commit of source

---
+++ src/com/abercog/minecraft/acoghorseracing/ACOGHorseRacing.java
@@ -0,0 +1,15 @@
+package com.abercog.minecraft.acoghorseracing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ACOGHorseRacing extends JavaPlugin {
+	
+	public void onEnable(){
+		getLogger().info("onEnable has been invoked!");
+	}
+ 
+	public void onDisable(){
+		getLogger().info("onDisable has been invoked!");
+	}
+
+}
